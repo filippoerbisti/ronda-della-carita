d2a800aa77fa8c53bce94ab11970aa351afd9a82
filippoerbisti <filippo.erbisti@gmail.com>
2022-02-14 17:04:07 +0100

    add col admin_confirm

--- back-office/database/migrations/2014_10_12_000000_create_users_table.php
+++ back-office/database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('cognome')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->boolean('admin_confirm');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
