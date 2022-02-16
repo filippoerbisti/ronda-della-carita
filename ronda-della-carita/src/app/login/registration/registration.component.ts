f64b82b6dadfa2b860907536b2cab8c852c92f4e
filippoerbisti <filippo.erbisti@gmail.com>
2022-02-17 00:21:09 +0100

    add snackbar registration ok

--- ronda-della-carita/src/app/login/registration/registration.component.ts
+++ ronda-della-carita/src/app/login/registration/registration.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
-import { FormControl, FormGroupDirective, NgForm, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, Validators, FormBuilder, FormGroup, ValidatorFn, AbstractControl, ValidationErrors } from '@angular/forms';
 import { AuthService } from '../../shared/service/auth.service';
 import { ErrorStateMatcher } from '@angular/material/core';
 import { Router } from '@angular/router';
 import { IUser } from '../../shared/interface/iuser';
+import { MatSnackBar, MatSnackBarHorizontalPosition } from '@angular/material/snack-bar';
 
 /* Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcherEmail implements ErrorStateMatcher {
@@ -43,31 +44,32 @@ export class RegistrationComponent implements OnInit {
 
   isLoading = false;
 
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  durationInSeconds = 10;
+
   constructor(
     public dialog: MatDialog,
     private router: Router,
     public fb: FormBuilder,
-    public authService: AuthService
+    public authService: AuthService,
+    private snackBar: MatSnackBar
   ) {
     this.registerForm = this.fb.group({
       nome: ['', [Validators.required]],
       cognome: ['', [Validators.required]],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
-      password_confirmation: [''],
+      password: ['', [Validators.required, this.checkPasswords]],
+      password_confirmation: ['', [this.checkPasswords]],
       admin_confirm: false
-    }),
-    { validator: this.checkPasswords }
+    })
   }
 
   ngOnInit(): void {
   }
 
-  checkPasswords(group: FormGroup) {
-    // here we have the 'passwords' group
-    let password = this.registerForm.value.password;
-    let password_confirmation = this.registerForm.value.password_confirmation;
-    // let password_confirmation = group.controls['password_confirmation'].value;
+  checkPasswords: ValidatorFn = (group: AbstractControl): ValidationErrors | null => {
+    let password = group.get('password')?.value;
+    let password_confirmation = group.get('password_confirmation')?.value;
 
     return password === password_confirmation ? null : { notSame: true };
   }
@@ -83,6 +85,10 @@ export class RegistrationComponent implements OnInit {
       () => {
         this.registerForm.reset();
         this.router.navigate(['/login']);
+        this.snackBar.open("Registrazione avvenuta on successo! `</br>` In attesa dell'amministratore, la contatteremo via mail quando tutto sarà pronto.", 'OK', {
+          horizontalPosition: this.horizontalPosition,
+          duration: this.durationInSeconds * 1000
+        })
       }
     )
   }
