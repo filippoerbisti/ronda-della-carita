28467d39762a3bdeaf72c281d841b4ecc05441da
Filippo Erbisti <80033304+filippoerbisti@users.noreply.github.com>
2022-05-28 14:34:00 +0200

    NB. COMMIT APPOSTA PER ROMPERE L’APPLICAZIONE

--- ronda-della-carita/src/app/app-routing.module.ts
+++ ronda-della-carita/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { LoginComponent } from './login/login.component';
+//import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './utils/page-not-found/page-not-found.component';
 
 import { HomeComponent } from './home/home.component';
